21426a17c049972b2c568be667ff5a0603277b77
yiyiq <yiyiqiu2004@gmail.com>
2025-05-31 09:35:47 +0200

    added docstrings

--- advanced_point.py
+++ advanced_point.py
@@ -8,6 +8,12 @@ class AdvancedPoint(ColorPoint):  # this means we are inheriting from ColorPoint
     COLORS = ["red", "green", "blue", "black", "white"]
 
     def __init__(self, x, y, color):
+        """
+        Magic method to initialize an advanced point with validation for coordinates and color.
+        :param x: x value
+        :param y: y value
+        :param color: color of point
+        """
         if not isinstance(x, (int, float)):  # this is a tuple, like a list, but we cannot change it
             raise TypeError("x must be a number")
         if not isinstance(y, (int, float)):
@@ -21,32 +27,65 @@ class AdvancedPoint(ColorPoint):  # this means we are inheriting from ColorPoint
 
     @property
     def x(self):
+        """
+        Gets x-coordinate value as an attribute instead of a method
+        :return: x-coordinate value
+        """
         return self._x
 
     @property
     def y(self):
+        """
+        Gets y-coordinate value as an attribute instead of a method
+        :return: x-coordinate value
+        """
         return self._y
 
     @property
     def color(self):
+        """
+        Gets the current color of the point
+        :return:current color of point as str
+        """
         return self._color
 
     @color.setter
     def color(self, new_color):
+        """
+        Sets a new color for the point with validation.
+        :param new_color: new color to assign to point
+        :return: point with new color
+        """
         if new_color not in AdvancedPoint.COLORS:
             raise ValueError(f"color must be one of: {AdvancedPoint.COLORS}")
         self._color = new_color
 
     @classmethod
     def add_color(cls, new_color):
+        """
+        Adds a new color to the class-level COLORS list.
+        :param new_color: new color to add to list
+        :return: list appended
+        """
         cls.COLORS.append(new_color)
 
     @staticmethod
     def distance_2_points(p1, p2):  # no need to reference the 'self'
+        """
+        Calculates distance between two points.
+        :param p1: First point (p1)
+        :param p2: Second point (p2)
+        :return: distance between p1 and p2
+        """
         return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
 
     @staticmethod
     def from_dictionary(dict):
+        """
+        Alternate constructor that creates an advanced point from a dictionary
+        :param dict: dictionary containing keys 'x', 'y' and 'color'
+        :return: new AdvancedPoint instance
+        """
         x = dict.get("x", 10)
         y = dict.get("y", 20)
         color = dict.get("color", "black")
